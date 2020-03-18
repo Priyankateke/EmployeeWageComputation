4a3a017aecd95b3c6378baae027b6817659c50fd
Priyankateke <priiyankateke1705@gmail.com>
2020-03-18 18:16:14 +0530

    ADDED: Calculated PartTime And FullTime Wage Using Case

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -2,21 +2,20 @@
 echo "Welcome To Employee Wage Computation"
 
 #constants
-readonly IS_EMP_PRESENT=1
 readonly IS_PART_TIME=1
 readonly IS_FULL_TIME=2
 readonly EMP_RATE_PER_HRS=20
 
-empAttendance=$(( RANDOM%2 ))
-
-if [ $IS_EMP_PRESENT -eq $empAttendance ]; then
-	empCheck=$(( RANDOM%3 ))
-	if [ $empCheck -eq $IS_PART_TIME ]; then
+empCheck=$(( RANDOM%3 ))
+case $empCheck in
+	$IS_PART_TIME)
 		empHours=4
-	elif [ $empCheck -eq $IS_FULL_TIME ]; then
+		;;
+	$IS_FULL_TIME)
 		empHours=8
-	fi
-else
-	empHours=0
-fi
+		;;
+	*)
+		empHours=0
+		;;
+esac
 salary=$(( $empHours*EMP_RATE_PER_HRS ))
