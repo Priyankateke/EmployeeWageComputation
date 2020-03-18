57eeed5f812cf16db86c45ab42a9fdf46091f305
Priyankateke <priiyankateke1705@gmail.com>
2020-03-18 18:46:33 +0530

    ADDED: Calculated Employee Wages For Month

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,17 +5,25 @@ echo "Welcome To Employee Wage Computation"
 readonly IS_PART_TIME=1
 readonly IS_FULL_TIME=2
 readonly EMP_RATE_PER_HRS=20
+readonly NUM_OF_WORKING_DAYS=20
 
-empCheck=$(( RANDOM%3 ))
-case $empCheck in
-	$IS_PART_TIME)
-		empHours=4
-		;;
-	$IS_FULL_TIME)
-		empHours=8
-		;;
-	*)
-		empHours=0
-		;;
-esac
-salary=$(( $empHours*EMP_RATE_PER_HRS ))
+#variables
+totalSalary=0
+
+for(( dayCount=1; dayCount<=NUM_OF_WORKING_DAYS; dayCount++ ))
+do
+	empCheck=$(( RANDOM%3 ))
+	case $empCheck in
+		$IS_PART_TIME)
+			empHours=4
+			;;
+		$IS_FULL_TIME)
+			empHours=8
+			;;
+		*)
+			empHours=0
+			;;
+	esac
+	perDaySalary=$(( $empHours*EMP_RATE_PER_HRS ))
+	totalSalary=$((totalSalary+perDaySalary))
+done
