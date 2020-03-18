460e0a6e42793906dc20e28d2af3b7766fef7915
Priyankateke <priiyankateke1705@gmail.com>
2020-03-18 17:48:14 +0530

    ADDED: Calculated Employee Wage According To PartTime And FullTime

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -3,13 +3,20 @@ echo "Welcome To Employee Wage Computation"
 
 #constants
 readonly IS_EMP_PRESENT=1
+readonly IS_PART_TIME=1
+readonly IS_FULL_TIME=2
+readonly EMP_RATE_PER_HRS=20
 
 empAttendance=$(( RANDOM%2 ))
 
-if [ $IS_EMP_PRESENT -eq $empAttendance ]; then 
-	empRatePerHrs=20 
-	empHours=8 
-	salary=$((empHours*empRatePerHrs))
+if [ $IS_EMP_PRESENT -eq $empAttendance ]; then
+	empCheck=$(( RANDOM%3 ))
+	if [ $empCheck -eq $IS_PART_TIME ]; then
+		empHours=4
+	elif [ $empCheck -eq $IS_FULL_TIME ]; then
+		empHours=8
+	fi
 else
-	salary=0
+	empHours=0
 fi
+salary=$(( $empHours*EMP_RATE_PER_HRS ))
